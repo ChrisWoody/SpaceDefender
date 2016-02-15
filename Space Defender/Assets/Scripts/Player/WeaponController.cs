8e2e9bc989731f41f07a1b3db53c2799b66d41e6
Chris Wood <chris.woody@live.com.au>
2016-02-15 20:34:58 +0800

    0.1 added weapon controller logic for player from previous game, in preparation for adding weapons

---
+++ Space Defender/Assets/Scripts/Player/WeaponController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public class WeaponController : MonoBehaviour
+    {
+        private IDictionary<int, WeaponModuleBase> _weaponModules;
+
+        private WeaponModuleBase SelectedWeapon
+        {
+            get { return _weaponModules[_selectedWeaponNumber]; }
+        }
+
+        private int _selectedWeaponNumber = 1;
+
+        private bool _paused;
+        private bool _frameAfterPaused;
+
+        void Start()
+        {
+            _weaponModules = new Dictionary<int, WeaponModuleBase>
+            {
+
+            };
+
+            //UiController.Pausing += OnPausing;
+            //UiController.Resuming += OnResuming;
+
+            //UiController.SetSelectedWeapon(SelectedWeapon.Name);
+        }
+
+        void Update()
+        {
+            if (_paused)
+                return;
+
+            if (_frameAfterPaused) // after resuming, the mouse button up occurs TODO: havent check this yet in new code...
+            {
+                _frameAfterPaused = false;
+                return;
+            }
+
+            UpdateSelectedWeapon();
+
+            foreach (var weaponModule in _weaponModules.Values)
+                weaponModule.OnAlwaysUpdate();
+
+            SelectedWeapon.OnSelectedUpdate();
+
+            if (Input.GetMouseButtonDown(0))
+                SelectedWeapon.OnLeftMouseButtonDown();
+            if (Input.GetMouseButton(0))
+                SelectedWeapon.OnLeftMouseButton();
+            if (Input.GetMouseButtonUp(0))
+                SelectedWeapon.OnLeftMouseButtonUp();
+
+            if (Input.GetMouseButtonDown(1))
+                SelectedWeapon.OnRightMouseButtonDown();
+            if (Input.GetMouseButton(1))
+                SelectedWeapon.OnRightMouseButton();
+            if (Input.GetMouseButtonUp(1))
+                SelectedWeapon.OnRightMouseButtonUp();
+        }
+
+        private void UpdateSelectedWeapon()
+        {
+            var previousWeaponNumber = _selectedWeaponNumber;
+
+            foreach (var numberKeyCode in PlayerHelper.NumberKeyCodes.Where(Input.GetKeyDown))
+                _selectedWeaponNumber = int.Parse(numberKeyCode.ToString().Substring(5, 1));
+
+            if (previousWeaponNumber == _selectedWeaponNumber || !_weaponModules.ContainsKey(_selectedWeaponNumber))
+            {
+                _selectedWeaponNumber = previousWeaponNumber;
+                return;
+            }
+
+            _weaponModules[previousWeaponNumber].OnUnselected();
+            _weaponModules[_selectedWeaponNumber].OnSelected();
+
+            //UiController.SetSelectedWeapon(SelectedWeapon.Name);
+        }
+
+        //private void OnPausing()
+        //{
+        //    _paused = true;
+        //}
+
+        //private void OnResuming()
+        //{
+        //    _paused = false;
+        //    _frameAfterPaused = true;
+        //}
+    }
+
+    public static class PlayerHelper
+    {
+        public static readonly KeyCode[] NumberKeyCodes =
+        {
+            KeyCode.Alpha0, KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4,
+            KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8, KeyCode.Alpha9
+        };
+    }
+}
---
+++ Space Defender/Assets/Scripts/Player/WeaponModuleBase.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Player
+{
+    public abstract class WeaponModuleBase
+    {
+        // TODO: look at adding 'cooldown' and 'charging' logic here, as each weapon module is using these.
+
+        protected Transform PlayerCamera;
+
+        protected Vector3 CameraPosition
+        {
+            get { return PlayerCamera.position; }
+        }
+
+        protected Vector3 CameraForward
+        {
+            get { return PlayerCamera.forward; }
+        }
+
+        protected WeaponModuleBase()
+        {
+            PlayerCamera = Camera.main.transform;
+        }
+
+        public abstract string Name { get; }
+
+        protected bool IsSelected { get; private set; }
+
+        public virtual void OnSelected()
+        {
+            IsSelected = true;
+        }
+
+        public virtual void OnUnselected()
+        {
+            IsSelected = false;
+        }
+
+        /// <summary>
+        /// Called every frame only if weapon is selected.
+        /// </summary>
+        public virtual void OnSelectedUpdate() { }
+
+        /// <summary>
+        /// Called every frame regardless if selected or not. Useful for 'cooldown' logic.
+        /// </summary>
+        public virtual void OnAlwaysUpdate() { }
+
+        public virtual void OnLeftMouseButtonDown() { }
+        public virtual void OnLeftMouseButton() { }
+        public virtual void OnLeftMouseButtonUp() { }
+
+        public virtual void OnRightMouseButtonDown() { }
+        public virtual void OnRightMouseButton() { }
+        public virtual void OnRightMouseButtonUp() { }
+
+        public virtual void OnPausing() { }
+        public virtual void OnResuming() { }
+    }
+}
